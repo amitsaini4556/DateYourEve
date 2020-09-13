206f6061745d3acf6170548a3094bce2be0dc5a2
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-13 13:58:27 +0530

    define method for redirecting to sign up screen

--- app/src/main/java/com/example/dateyoureve/Sign_In.java
+++ app/src/main/java/com/example/dateyoureve/Sign_In.java
@@ -1,8 +1,10 @@
 package com.example.dateyoureve;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Sign_In extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Sign_In extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__in);
+    }
+
+    public void signup(View view) {
+        Intent intant = new Intent(Sign_In.this,SignupActivity.class);
+        startActivity(intant);
     }
 }
