8e48b09638637dca99921417118571fa2ff09ccc
amitsaini4556 <amitsaini2701@gmail.com>
2020-10-15 08:18:30 +0530

    optimize validation code with help of InputValidation.java by passing signup object to InputValidation class

--- app/src/main/java/com/example/dateyoureve/SignupActivity.java
+++ app/src/main/java/com/example/dateyoureve/SignupActivity.java
@@ -1,192 +1,48 @@
 package com.example.dateyoureve;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.regex.Pattern;
-
 public class SignupActivity extends AppCompatActivity{
-    private static final Pattern PASSWORD_PATTERN =
-            Pattern.compile("^" +
-                    //"(?=.*[0-9])" +         //at least 1 digit
-                    //"(?=.*[a-z])" +         //at least 1 lower case letter
-                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
-                    "(?=.*[a-zA-Z])" +      //any letter
-                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
-                    "(?=\\S+$)" +           //no white spaces
-                    ".{4,}" +               //at least 4 characters
-                    "$");
-    private static final Pattern USER_NAME =
-            Pattern.compile("^" +
-                    "(?=.*[a-zA-Z])" +      //any letter
-                    "(?=\\S+$)" +           //no white spaces
-                    ".{4,}" +               //at least 4 characters
-                    "$");
-    private static final Pattern VALID_PHONE =
-            Pattern.compile("^" +
-                    "(?=\\S+$)" +           //no white spaces
-                    ".{10,}" +               //at least 10 characters
-                    "$");
-    private TextInputLayout textInputEmail;
-    private TextInputLayout textInputPassword;
-    private TextInputLayout textInputUser;
-    private TextInputLayout textInputPhone;
-    private Button btnToggleDark;
+    UserLocation userLocation;
+    TextInputLayout textInputEmail;
+    TextInputLayout textInputPassword;
+    TextInputLayout textInputUser;
+    TextInputLayout textInputPhone;
+    Button btnToggleDark;
+    Double latitude, longit;
+    String country,locality,addr;
+
+    FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        new UserLocation(this);
         btnToggleDark  = findViewById(R.id.darkmode);
-        // Saving state of our app
-        // using SharedPreferences
-        SharedPreferences sharedPreferences
-                = getSharedPreferences(
-                "sharedPrefs", MODE_PRIVATE);
-        final SharedPreferences.Editor editor
-                = sharedPreferences.edit();
-        final boolean isDarkModeOn
-                = sharedPreferences
-                .getBoolean(
-                        "isDarkModeOn", false);
-
-        // When user reopens the app
-        // after applying dark/light mode
-        if (isDarkModeOn) {
-            AppCompatDelegate
-                    .setDefaultNightMode(
-                            AppCompatDelegate
-                                    .MODE_NIGHT_YES);
-            btnToggleDark.setText(
-                    "Disable Dark Mode");
-        }
-        else {
-            AppCompatDelegate
-                    .setDefaultNightMode(
-                            AppCompatDelegate
-                                    .MODE_NIGHT_NO);
-            btnToggleDark
-                    .setText(
-                            "Enable Dark Mode");
-        }
-
-        btnToggleDark.setOnClickListener(
-                new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View view)
-                    {
-                        // When user taps the enable/disable
-                        // dark mode button
-                        if (isDarkModeOn) {
-
-                            // if dark mode is on it
-                            // will turn it off
-                            AppCompatDelegate
-                                    .setDefaultNightMode(
-                                            AppCompatDelegate
-                                                    .MODE_NIGHT_NO);
-                            // it will set isDarkModeOn
-                            // boolean to false
-                            editor.putBoolean(
-                                    "isDarkModeOn", false);
-                            editor.apply();
-
-                            // change text of Button
-                            btnToggleDark.setText(
-                                    "Enable Dark Mode");
-                        }
-                        else {
-
-                            // if dark mode is off
-                            // it will turn it on
-                            AppCompatDelegate
-                                    .setDefaultNightMode(
-                                            AppCompatDelegate
-                                                    .MODE_NIGHT_YES);
-
-                            // it will set isDarkModeOn
-                            // boolean to true
-                            editor.putBoolean(
-                                    "isDarkModeOn", true);
-                            editor.apply();
-
-                            // change text of Button
-                            btnToggleDark.setText(
-                                    "Disable Dark Mode");
-                        }
-                    }
-                });
+        new DarkMode(this);
 
         textInputEmail = findViewById(R.id.text_input_email);
         textInputPassword = findViewById(R.id.text_input_password);
         textInputUser = findViewById(R.id.text_input_username);
         textInputPhone = findViewById(R.id.text_input_phone);
     }
-    private boolean validateEmail() {
-        String emailInput = textInputEmail.getEditText().getText().toString().trim();
-        if (emailInput.isEmpty()) {
-            textInputEmail.setError("Field can't be empty");
-            return false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
-            textInputEmail.setError("Please enter a valid email address");
-            return false;
-        } else {
-            textInputEmail.setError(null);
-            return true;
-        }
-    }
-    private boolean validateUsername() {
-        String userInput = textInputUser.getEditText().getText().toString().trim();
-        if (userInput.isEmpty()) {
-            textInputUser.setError("Field can't be empty");
-            return false;
-        } else if (!USER_NAME.matcher(userInput).matches()) {
-            textInputUser.setError("Please enter a valid username");
-            return false;
-        } else {
-            textInputUser.setError(null);
-            return true;
-        }
-    }
-    private boolean validatePhone() {
-        String phoneInput = textInputPhone.getEditText().getText().toString().trim();
-        if (phoneInput.isEmpty()) {
-            textInputPhone.setError("Field can't be empty");
-            return false;
-        } else if (!VALID_PHONE.matcher(phoneInput).matches()) {
-            textInputPhone.setError("Please enter a valid mobile no.");
-            return false;
-        } else {
-            textInputPhone.setError(null);
-            return true;
-        }
-    }
-    private boolean validatePassword() {
-        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
-        if (passwordInput.isEmpty()) {
-            textInputPassword.setError("Field can't be empty");
-            return false;
-        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
-            textInputPassword.setError("Password too weak");
-            return false;
-        } else {
-            textInputPassword.setError(null);
-            return true;
-        }
-    }
-    public void confirmInput(View v) {
-        if (!validateEmail() | !validatePassword() | !validateUsername() | !validatePhone()) {
+
+    public void confirmInput(View v)  {
+        final InputValidation inputValidation = new InputValidation(this);
+        if (!inputValidation.validateEmail() | !inputValidation.validatePassword() | !inputValidation.validateUsername() | !inputValidation.validatePhone()) {
             return;
         }
         String input = "Email: " + textInputEmail.getEditText().getText().toString();
@@ -194,6 +50,11 @@ public class SignupActivity extends AppCompatActivity{
         input += "Password: " + textInputPassword.getEditText().getText().toString();
         Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
         Intent intant = new Intent(SignupActivity.this,Home.class);
+        intant.putExtra("latitude", latitude);
+        intant.putExtra("longit", longit);
+        intant.putExtra("country", country);
+        intant.putExtra("locality", locality);
+        intant.putExtra("addr", addr);
         startActivity(intant);
         this.finish();
     }
