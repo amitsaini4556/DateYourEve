d37b5fe834a5691862a706da2b3d68daac747adf
amitsaini4556 <amitsaini2701@gmail.com>
2020-10-15 08:49:45 +0530

    DarkMode Enable/Disable according to users request or use shared variable, extends the signupActivity

---
+++ app/src/main/java/com/example/dateyoureve/DarkMode.java
@@ -0,0 +1,47 @@
+package com.example.dateyoureve;
+
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class DarkMode{
+    Button btnToggleDark;
+    SignupActivity signupActivity;
+    DarkMode(final SignupActivity signupActivity){
+        this.signupActivity = signupActivity;
+        SharedPreferences sharedPreferences = signupActivity.getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        final boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
+
+        if (isDarkModeOn) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            signupActivity.btnToggleDark.setText("Disable Dark Mode");
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            signupActivity.btnToggleDark.setText("Enable Dark Mode");
+        }
+
+        signupActivity.btnToggleDark.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        if (isDarkModeOn) {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                            editor.putBoolean("isDarkModeOn", false);
+                            editor.apply();
+                            signupActivity.btnToggleDark.setText("Enable Dark Mode");
+                        } else {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                            editor.putBoolean("isDarkModeOn", true);
+                            editor.apply();
+                            signupActivity.btnToggleDark.setText("Disable Dark Mode");
+                        }
+                    }
+                });
+    }
+}
