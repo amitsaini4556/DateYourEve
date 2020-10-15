12b97d98faa539085f11b163070035ea28644e44
amitsaini4556 <amitsaini2701@gmail.com>
2020-10-15 08:16:52 +0530

    optimize validation code with help of separate InputValidation.java by passing sign_in object

--- app/src/main/java/com/example/dateyoureve/Sign_In.java
+++ app/src/main/java/com/example/dateyoureve/Sign_In.java
@@ -2,7 +2,6 @@ package com.example.dateyoureve;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
@@ -10,21 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.regex.Pattern;
-
 public class Sign_In extends AppCompatActivity {
-    private static final Pattern PASSWORD_PATTERN =
-            Pattern.compile("^" +
-                    //"(?=.*[0-9])" +         //at least 1 digit
-                    //"(?=.*[a-z])" +         //at least 1 lower case letter
-                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
-                    "(?=.*[a-zA-Z])" +      //any letter
-                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
-                    "(?=\\S+$)" +           //no white spaces
-                    ".{4,}" +               //at least 4 characters
-                    "$");
-    private TextInputLayout textInputEmail;
-    private TextInputLayout textInputPassword;
+    TextInputLayout textInputEmail;
+    TextInputLayout textInputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,35 +20,12 @@ public class Sign_In extends AppCompatActivity {
 
         textInputEmail = findViewById(R.id.text_input_email);
         textInputPassword = findViewById(R.id.text_input_password);
+
     }
-    private boolean validateEmail() {
-        String emailInput = textInputEmail.getEditText().getText().toString().trim();
-        if (emailInput.isEmpty()) {
-            textInputEmail.setError("Field can't be empty");
-            return false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
-            textInputEmail.setError("Please enter a valid email address");
-            return false;
-        } else {
-            textInputEmail.setError(null);
-            return true;
-        }
-    }
-    private boolean validatePassword() {
-        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
-        if (passwordInput.isEmpty()) {
-            textInputPassword.setError("Field can't be empty");
-            return false;
-        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
-            textInputPassword.setError("Password too weak");
-            return false;
-        } else {
-            textInputPassword.setError(null);
-            return true;
-        }
-    }
+
     public void confirmInput(View v) {
-        if (!validateEmail() | !validatePassword()) {
+        InputValidation inputValidation = new InputValidation( this);
+        if (!inputValidation.validateEmailSignIn() | !inputValidation.validatePasswordSignIn()) {
             return;
         }
         String input = "Email: " + textInputEmail.getEditText().getText().toString();
