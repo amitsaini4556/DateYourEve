88158d9af12997fb27dc945e5462bc3aa57752b6
Hema bhagnani <hemabhagnani5@gmail.com>
2020-10-27 19:22:42 +0530

    redirect notification screen to event details

--- app/src/main/java/com/example/dateyoureve/ui/NotificationFragment.java
+++ app/src/main/java/com/example/dateyoureve/ui/NotificationFragment.java
@@ -1,20 +1,33 @@
 package com.example.dateyoureve.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.example.dateyoureve.EventDetails;
 import com.example.dateyoureve.R;
 
 
 public class NotificationFragment extends Fragment {
+    CardView cardView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_notification, container, false);
+        cardView = root.findViewById(R.id.cardview);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), EventDetails.class);
+                startActivity(intent);
+            }
+        });
+        return root;
     }
 }
