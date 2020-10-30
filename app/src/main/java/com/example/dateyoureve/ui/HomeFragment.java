05a10c3844bc707de45296438b27df758068a436
amitsaini4556 <amitsaini2701@gmail.com>
2020-10-30 16:56:44 +0530

    add MyHomeData class for recyclerView

--- app/src/main/java/com/example/dateyoureve/ui/HomeFragment.java
+++ app/src/main/java/com/example/dateyoureve/ui/HomeFragment.java
@@ -1,6 +1,5 @@
 package com.example.dateyoureve.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,8 +14,11 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.dateyoureve.EventDetails;
+import com.example.dateyoureve.MyHomeAdapter;
+import com.example.dateyoureve.MyHomeData;
 import com.example.dateyoureve.R;
 
 
@@ -40,29 +42,46 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         cardView = root.findViewById(R.id.cardview);
-        lati = root.findViewById(R.id.lati);
-        longi = root.findViewById(R.id.longi);
-        cur = root.findViewById(R.id.cur);
-        loc = root.findViewById(R.id.loc);
-        add = root.findViewById(R.id.add);
-        lati.setText(s1);
-        longi.setText(s2);
-        cur.setText(s3);
-        loc.setText(s4);
-        add.setText(s5);
-        cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getContext(),EventDetails.class);
-                startActivity(intent);
-            }
-        });
+//        lati = root.findViewById(R.id.lati);
+//        longi = root.findViewById(R.id.longi);
+//        cur = root.findViewById(R.id.cur);
+//        loc = root.findViewById(R.id.loc);
+//        add = root.findViewById(R.id.add);
+//        lati.setText(s1);
+//        longi.setText(s2);
+//        cur.setText(s3);
+//        loc.setText(s4);
+//        add.setText(s5);
+//        cardView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getContext(),EventDetails.class);
+//                startActivity(intent);
+//            }
+//        });
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 //textView.setText(s);
             }
         });
+
+
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        MyHomeData[] myHomeData = new MyHomeData[]{
+              new MyHomeData("Ecell, CTAE","To connect with entrepreneurial leaders, innovators and alumni of CTAE, Udaipur and engage them to foster the same passion and spirit in the current breed of students .","20/10/2020","CSE departmnet",R.drawable.download),
+              new MyHomeData("Online Webinar","In this time of crisis we came across many students who have doubts regarding what next? Amongst many internship opportunities being cancelled, no sign of job interviews, even examination being delayed, we all have found ourselves wondering how can we utilise this time?","21/10/2020","CSE departmnet",R.drawable.i1),
+              new MyHomeData("Something About ML","Machine learning is an application of artificial intelligence (AI) that provides systems the ability to automatically learn and improve from experience without being explicitly programmed. Machine learning focuses on the development of computer programs that can access data and use it learn for themselves","22/10/2020","CSE departmnet",R.drawable.i2),
+              new MyHomeData("Programming...","Programming is the process of creating a set of instructions that tell a computer how to perform a task. Programming can be done using a variety of computer programming languages, such as JavaScript, Python, and C++. Created by Pamela Fox.","23/10/2020","CSE departmnet",R.drawable.i3),
+              new MyHomeData("PrePlacement Talk","Pre-placement talks or we call it as the discussion about the company .Here we came to know about various aspects of a company and it tells us that if we are suitable enough to work for a company.","24/10/2020","CSE departmnet",R.drawable.i4),
+        };
+        MyHomeAdapter myHomeAdapter = new MyHomeAdapter(myHomeData,HomeFragment.this);
+        recyclerView.setAdapter(myHomeAdapter);
+
+
+
         return root;
     }
     @Override
