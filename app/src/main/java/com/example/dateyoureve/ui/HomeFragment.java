03c1ef17cd6f654dafee741fd56793cb72a127de
amitsaini4556 <amitsaini2701@gmail.com>
2020-11-02 19:09:18 +0530

    add searchView for filer the data at Home Screen

--- app/src/main/java/com/example/dateyoureve/ui/HomeFragment.java
+++ app/src/main/java/com/example/dateyoureve/ui/HomeFragment.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -21,11 +21,13 @@ import com.example.dateyoureve.MyHomeAdapter;
 import com.example.dateyoureve.MyHomeData;
 import com.example.dateyoureve.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HomeFragment extends Fragment {
-    CardView cardView;
     private HomeViewModel homeViewModel;
-    TextView lati,longi,cur,loc,add;
+    private List<MyHomeData> myHomeData;;
     String s1,s2,s3,s4,s5;
     public HomeFragment(String s1, String s2, String s3, String s4, String s5){
         this.s1=s1;
@@ -34,31 +36,14 @@ public class HomeFragment extends Fragment {
         this.s4=s4;
         this.s5=s5;
     }
-    public HomeFragment () {}
-
+    public HomeFragment () {
+    }
+    MyHomeAdapter myHomeAdapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        cardView = root.findViewById(R.id.cardview);
-//        lati = root.findViewById(R.id.lati);
-//        longi = root.findViewById(R.id.longi);
-//        cur = root.findViewById(R.id.cur);
-//        loc = root.findViewById(R.id.loc);
-//        add = root.findViewById(R.id.add);
-//        lati.setText(s1);
-//        longi.setText(s2);
-//        cur.setText(s3);
-//        loc.setText(s4);
-//        add.setText(s5);
-//        cardView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getContext(),EventDetails.class);
-//                startActivity(intent);
-//            }
-//        });
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -70,18 +55,14 @@ public class HomeFragment extends Fragment {
         RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        MyHomeData[] myHomeData = new MyHomeData[]{
-              new MyHomeData("Ecell, CTAE","To connect with entrepreneurial leaders, innovators and alumni of CTAE, Udaipur and engage them to foster the same passion and spirit in the current breed of students .","20/10/2020","CSE departmnet",R.drawable.download),
-              new MyHomeData("Online Webinar","In this time of crisis we came across many students who have doubts regarding what next? Amongst many internship opportunities being cancelled, no sign of job interviews, even examination being delayed, we all have found ourselves wondering how can we utilise this time?","21/10/2020","CSE departmnet",R.drawable.i1),
-              new MyHomeData("Something About ML","Machine learning is an application of artificial intelligence (AI) that provides systems the ability to automatically learn and improve from experience without being explicitly programmed. Machine learning focuses on the development of computer programs that can access data and use it learn for themselves","22/10/2020","CSE departmnet",R.drawable.i2),
-              new MyHomeData("Programming...","Programming is the process of creating a set of instructions that tell a computer how to perform a task. Programming can be done using a variety of computer programming languages, such as JavaScript, Python, and C++. Created by Pamela Fox.","23/10/2020","CSE departmnet",R.drawable.i3),
-              new MyHomeData("PrePlacement Talk","Pre-placement talks or we call it as the discussion about the company .Here we came to know about various aspects of a company and it tells us that if we are suitable enough to work for a company.","24/10/2020","CSE departmnet",R.drawable.i4),
-        };
-        MyHomeAdapter myHomeAdapter = new MyHomeAdapter(myHomeData,HomeFragment.this);
+        myHomeData = new ArrayList<MyHomeData>();
+              myHomeData.add(new MyHomeData("Ecell, CTAE","To connect with entrepreneurial leaders, innovators and alumni of CTAE, Udaipur and engage them to foster the same passion and spirit in the current breed of students .","20/10/2020","CSE departmnet",R.drawable.download));
+              myHomeData.add(new MyHomeData("Online Webinar","In this time of crisis we came across many students who have doubts regarding what next? Amongst many internship opportunities being cancelled, no sign of job interviews, even examination being delayed, we all have found ourselves wondering how can we utilise this time?","21/10/2020","CSE departmnet",R.drawable.i1));
+              myHomeData.add(new MyHomeData("Something About ML","Machine learning is an application of artificial intelligence (AI) that provides systems the ability to automatically learn and improve from experience without being explicitly programmed. Machine learning focuses on the development of computer programs that can access data and use it learn for themselves","22/10/2020","CSE departmnet",R.drawable.i2));
+              myHomeData.add(new MyHomeData("Programming...","Programming is the process of creating a set of instructions that tell a computer how to perform a task. Programming can be done using a variety of computer programming languages, such as JavaScript, Python, and C++. Created by Pamela Fox.","23/10/2020","CSE departmnet",R.drawable.i3));
+              myHomeData.add(new MyHomeData("PrePlacement Talk","Pre-placement talks or we call it as the discussion about the company .Here we came to know about various aspects of a company and it tells us that if we are suitable enough to work for a company.","24/10/2020","CSE departmnet",R.drawable.i4));
+        myHomeAdapter = new MyHomeAdapter(myHomeData,HomeFragment.this);
         recyclerView.setAdapter(myHomeAdapter);
-
-
-
         return root;
     }
     @Override
@@ -93,8 +74,22 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // TODO Add your menu entries here
-        super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.appbar, menu);
+        MenuItem searchItem = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                myHomeAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
 }
