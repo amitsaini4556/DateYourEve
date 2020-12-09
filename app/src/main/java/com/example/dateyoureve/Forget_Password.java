7d68a9409ceb08db1b2d929b9cdc8e29d0e7145c
amitsaini4556 <amitsaini2701@gmail.com>
2020-12-09 08:22:56 +0530

    add backend for password reset

--- app/src/main/java/com/example/dateyoureve/Forget_Password.java
+++ app/src/main/java/com/example/dateyoureve/Forget_Password.java
@@ -1,18 +1,57 @@
 package com.example.dateyoureve;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Forget_Password extends AppCompatActivity {
+    Button forgetPassword;
+    TextInputLayout textInputEmail;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget__password);
+        forgetPassword = findViewById(R.id.forgetPassword);
         Toolbar toolbar=findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        textInputEmail = findViewById(R.id.emailText);
+        progressBar = findViewById(R.id.progressBar2);
+        mAuth = FirebaseAuth.getInstance();
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = textInputEmail.getEditText().getText().toString();
+                progressBar.setVisibility(View.VISIBLE);
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                }
+
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        });
+            }
+        });
     }
 }
