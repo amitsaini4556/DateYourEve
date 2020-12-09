252f1583d82c589c32a7d6251fabac85cc825bec
amitsaini4556 <amitsaini2701@gmail.com>
2020-12-09 08:25:08 +0530

    register new user and add user's data to firebase

--- app/src/main/java/com/example/dateyoureve/SignupActivity.java
+++ app/src/main/java/com/example/dateyoureve/SignupActivity.java
@@ -6,11 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity{
     SignupActivity signupActivity;
@@ -21,6 +29,9 @@ public class SignupActivity extends AppCompatActivity{
     Button btnToggleDark;
     Double latitude, longit;
     String country,locality,addr;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseReference;
 
     FusedLocationProviderClient mFusedLocationClient;
 
@@ -28,6 +39,9 @@ public class SignupActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference("users");
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         btnToggleDark  = findViewById(R.id.darkmode);
@@ -44,16 +58,35 @@ public class SignupActivity extends AppCompatActivity{
         if (!inputValidation.validateEmail() | !inputValidation.validatePassword() | !inputValidation.validateUsername() | !inputValidation.validatePhone()) {
             return;
         }
-        String input = "Email: " + textInputEmail.getEditText().getText().toString();
-        input += "\n";
-        input += "Password: " + textInputPassword.getEditText().getText().toString();
-        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+        registerUser(textInputEmail.getEditText().getText().toString(),textInputPassword.getEditText().getText().toString());
+
+    }
+    public void registerUser(String email,String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "done",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    public  void  updateUI(FirebaseUser currentUser)
+    {
+        User user = new User(textInputUser.getEditText().getText().toString(),textInputPassword.getEditText().getText().toString(),textInputEmail.getEditText().getText().toString(),textInputPhone.getEditText().getText().toString());
+        mDatabaseReference.child(currentUser.getUid()).setValue(user);
         Intent intant = new Intent(SignupActivity.this,Home.class);
-        intant.putExtra("latitude", latitude);
-        intant.putExtra("longit", longit);
-        intant.putExtra("country", country);
-        intant.putExtra("locality", locality);
-        intant.putExtra("addr", addr);
         startActivity(intant);
         this.finish();
     }
